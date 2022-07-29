2a79a1a16f49f9e76b463073a5924d2148d7f867
June060522 <a80336234@gmail.com>
2022-07-29 20:27:07 +0900

    attack
    player Attack
    enemy hp

---
+++ yesterday-shooting/Assets/02.Scripts/JUN/Hp/EnemyHp.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemyHp : MonoBehaviour, IDamageble
+{
+    [SerializeField] int hp;
+    public void OnDamage(Action lambda)
+    {
+        hp--;
+        lambda?.Invoke();
+        Debug.Log("데미지 받음");
+    }
+
+    private void Update()
+    {
+        if(hp <= 0)
+        {
+            Die();//여기에 죽는 애니메이션
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("PlayerBullet"))
+            OnDamage(()=>{});
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
