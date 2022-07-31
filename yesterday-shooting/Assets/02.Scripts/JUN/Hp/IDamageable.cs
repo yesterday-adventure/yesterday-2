0ff7c45ed8a43d1dc87d0da699292ad84cf1fa58
June060522 <a80336234@gmail.com>
2022-07-31 23:53:42 +0900

    Attack
    hp
    nekback
    balance

---
+++ yesterday-shooting/Assets/02.Scripts/JUN/Hp/IDamageable.cs
@@ -0,0 +1,6 @@
+using System;
+
+public interface IDamageable
+{
+    public void OnDamage(Action lambda);
+}
---
+++ yesterday-shooting/Assets/02.Scripts/JUN/Hp/MeleeMonster.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeMonster : MonoBehaviour
+{
+    [SerializeField]private Rigidbody2D Rb2D = null;
+    //[SerializeField]private Collision2D playerC = null;
+
+    private void Awake()
+    {
+        Rb2D = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        //playerC = GameObject.Find("Player").GetComponent<Collision2D>();
+    }
+    
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        Debug.Log("!23");
+        if(other.gameObject.tag == "Player")
+        {
+            Rb2D.AddForce(Vector2.up * 1);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        Debug.Log("1231342");
+        if(other.gameObject.tag == "Player")
+        {
+            Rb2D.velocity = Vector2.zero;
+        }    
+    }
+}
