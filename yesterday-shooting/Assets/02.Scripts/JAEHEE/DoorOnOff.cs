cdc4f907da745298b1544adc8303b24b2752331d
LAPTOP-5SS2J05M\user <likjhee1@gmail.com>
2022-08-03 18:20:23 +0900

    Door
    Add_Door

---
+++ yesterday-shooting/Assets/02.Scripts/JAEHEE/DoorOnOff.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DoorOnOff : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+
+    private void Update()
+    {
+        Debug.Log(door.activeSelf);
+        Collider2D hit = Physics2D.OverlapBox(transform.position, new Vector2(17.7f, 9.3f), 0, 1 << 10);
+        if (hit != null)
+        {
+            door.SetActive(true);
+        }
+        else
+        {
+            door.SetActive(false);
+        }
+    }
+
+}
