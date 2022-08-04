07907e15495cb99c8c3c655f7eecf1eb375591f0
LAPTOP-5SS2J05M\user <likjhee1@gmail.com>
2022-08-05 00:06:51 +0900

    FIx
    Fix

--- yesterday-shooting/Assets/02.Scripts/JAEHEE/EnterRoom.cs
+++ yesterday-shooting/Assets/02.Scripts/JAEHEE/EnterRoom.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class EnterRoom : MonoBehaviour
 {
-    [SerializeField]
-    List<GameObject> room;
 
     private void Update()
     {
-        Collider2D hit = Physics2D.OverlapBox(transform.position, new Vector2(17.55f, 9.25f), 0, 1 << 10);
-        Debug.Log(hit);
+        Collider2D hit = Physics2D.OverlapBox(transform.position, new Vector2(17.55f, 9.25f), 0, 1 << 11);
     }
 
 
--- yesterday-shooting/Assets/02.Scripts/JUN/CameraMove.cs
+++ yesterday-shooting/Assets/02.Scripts/JUN/CameraMove.cs
@@ -14,22 +14,22 @@ public class CameraMove : MonoBehaviour
         if(player.position.x - transform.position.x > offset.x)
         {
             transform.DOMove(new Vector3(transform.position.x + offset.x * 2,transform.position.y,-10),0.1f);
-            playerP.transform.position += new Vector3(1,0,0);
+            //playerP.transform.position += new Vector3(1,0,0);
         }
         if(player.position.x - transform.position.x < -offset.x)
         {
             transform.DOMove(new Vector3(transform.position.x - offset.x * 2,transform.position.y,-10),0.1f);
-            playerP.transform.position += new Vector3(-1,0,0);
+            //playerP.transform.position += new Vector3(-1,0,0);
         }
         if(player.position.y - transform.position.y > offset.y)
         {
             transform.DOMove(new Vector3(transform.position.x,transform.position.y + offset.y * 2,-10),0.1f);
-            playerP.transform.position += new Vector3(0,1,0);
+            //playerP.transform.position += new Vector3(0,1,0);
         }
         if(player.position.y - transform.position.y < -offset.y)
         {
             transform.DOMove(new Vector3(transform.position.x,transform.position.y - offset.y * 2,-10),0.1f);
-            playerP.transform.position += new Vector3(0,-1,0);
+            //playerP.transform.position += new Vector3(0,-1,0);
         }
     }
 }
