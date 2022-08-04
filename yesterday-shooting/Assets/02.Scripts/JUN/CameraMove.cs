7732d721c6b8ef9c02fb478dbc65f137ccd19a15
LAPTOP-5SS2J05M\user <likjhee1@gmail.com>
2022-08-05 02:43:16 +0900

    .
    .

--- yesterday-shooting/Assets/02.Scripts/JUN/CameraMove.cs
+++ yesterday-shooting/Assets/02.Scripts/JUN/CameraMove.cs
@@ -9,27 +9,28 @@ public class CameraMove : MonoBehaviour
     public Vector3 offset;
 
     public GameObject playerP;
+    
     void Update()
     {
         if(player.position.x - transform.position.x > offset.x)
         {
+            playerP.transform.position += new Vector3(1,0,0);
             transform.DOMove(new Vector3(transform.position.x + offset.x * 2,transform.position.y,-10),0.1f);
-            //playerP.transform.position += new Vector3(1,0,0);
         }
         if(player.position.x - transform.position.x < -offset.x)
         {
+            playerP.transform.position += new Vector3(-1,0,0);
             transform.DOMove(new Vector3(transform.position.x - offset.x * 2,transform.position.y,-10),0.1f);
-            //playerP.transform.position += new Vector3(-1,0,0);
         }
         if(player.position.y - transform.position.y > offset.y)
         {
+            playerP.transform.position += new Vector3(0,1,0);
             transform.DOMove(new Vector3(transform.position.x,transform.position.y + offset.y * 2,-10),0.1f);
-            //playerP.transform.position += new Vector3(0,1,0);
         }
         if(player.position.y - transform.position.y < -offset.y)
         {
+            playerP.transform.position += new Vector3(0,-1,0);
             transform.DOMove(new Vector3(transform.position.x,transform.position.y - offset.y * 2,-10),0.1f);
-            //playerP.transform.position += new Vector3(0,-1,0);
         }
     }
 }
