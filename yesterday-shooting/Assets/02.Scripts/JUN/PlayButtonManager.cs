7505352d6f5dbb399eee26a6a2100943f4bddf33
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2022-08-05 16:24:55 +0900

    방 클리어 유무 확인 기능

--- yesterday-shooting/Assets/02.Scripts/JUN/PlayButtonManager.cs
+++ yesterday-shooting/Assets/02.Scripts/JUN/PlayButtonManager.cs
@@ -8,13 +8,9 @@ public class PlayButtonManager : MonoBehaviour
 {
     [SerializeField] GameObject stopPanel;
     [SerializeField] GameObject player;
-    //[SerializeField] GameObject cameraP;
 
     private void Start()
     {
-        //player.transform.position = DataManager.instance.nowPlayer.playerPosition;
-        //cameraP.transform.position = DataManager.instance.nowPlayer.cameraPosition;
-
         switch (DataManager.instance.nowPlayer.playerRoom)
         {
             case 0:
@@ -68,9 +64,6 @@ public class PlayButtonManager : MonoBehaviour
 
     public void OnClickExit()
     {
-        //DataManager.instance.nowPlayer.playerPosition = player.transform.position;
-        //DataManager.instance.nowPlayer.cameraPosition = cameraP.transform.position;
-        //DataManager.instance.SaveData();
         SceneManager.LoadScene("Intro");
         Time.timeScale = 1;
     }
