044d5628222e587cb02c0ae0ca3405f935c93a1d
June060522 <a80336234@gmail.com>
2022-08-05 13:06:57 +0900

    camera bug fix

--- yesterday-shooting/Assets/02.Scripts/JUN/CameraMove.cs
+++ yesterday-shooting/Assets/02.Scripts/JUN/CameraMove.cs
@@ -9,28 +9,88 @@ public class CameraMove : MonoBehaviour
     public Vector3 offset;
 
     public GameObject playerP;
-    
+    private bool move = true;
     void Update()
     {
-        if(player.position.x - transform.position.x > offset.x)
+        if(player.position.x - transform.position.x > offset.x && move)
         {
-            playerP.transform.position += new Vector3(1,0,0);
-            transform.DOMove(new Vector3(transform.position.x + offset.x * 2,transform.position.y,-10),0.1f);
+            StartCoroutine(CameraRight());
+            move = false;
+            //transform.DOMove(new Vector3(transform.position.x + offset.x * 2,transform.position.y,-10),0.1f,true);;
+            //playerP.transform.position += new Vector3(1,0,0);
         }
-        if(player.position.x - transform.position.x < -offset.x)
+        if(player.position.x - transform.position.x < -offset.x && move)
         {
-            playerP.transform.position += new Vector3(-1,0,0);
-            transform.DOMove(new Vector3(transform.position.x - offset.x * 2,transform.position.y,-10),0.1f);
+            StartCoroutine(CameraLeft());
+            move = false;
+            //transform.DOMove(new Vector3(transform.position.x - offset.x * 2,transform.position.y,-10),0.1f,true);
+            //playerP.transform.position += new Vector3(-1,0,0);
         }
-        if(player.position.y - transform.position.y > offset.y)
+        if(player.position.y - transform.position.y > offset.y && move)
         {
-            playerP.transform.position += new Vector3(0,1,0);
-            transform.DOMove(new Vector3(transform.position.x,transform.position.y + offset.y * 2,-10),0.1f);
+            StartCoroutine(CameraUp());
+            move = false;
+            //transform.DOMove(new Vector3(transform.position.x,transform.position.y + offset.y * 2,-10),0.1f,true);
+            //playerP.transform.position += new Vector3(0,1,0);
         }
-        if(player.position.y - transform.position.y < -offset.y)
+        if(player.position.y - transform.position.y < -offset.y && move)
         {
-            playerP.transform.position += new Vector3(0,-1,0);
-            transform.DOMove(new Vector3(transform.position.x,transform.position.y - offset.y * 2,-10),0.1f);
+            StartCoroutine(CameraDown());
+            move = false;
+            //transform.DOMove(new Vector3(transform.position.x,transform.position.y - offset.y * 2,-10),0.1f,true);
+            //playerP.transform.position += new Vector3(0,-1,0);
         }
+    }
+
+    IEnumerator CameraRight()
+    {
+        float x = transform.position.x;
+        while(transform.position.x != x + offset.x * 2)
+        {
+            transform.position += new Vector3(1,0,0);
+            yield return null;
+        }
+        playerP.transform.position += new Vector3(1,0,0);
+        yield return new WaitForSeconds(0.1f);
+        move = true;
+    }
+
+    IEnumerator CameraLeft()
+    {
+        float x = transform.position.x;
+        while(transform.position.x != x - offset.x * 2)
+        {
+            transform.position += new Vector3(-1,0,0);
+            yield return null;
+        }
+        playerP.transform.position += new Vector3(-1,0,0);
+        yield return new WaitForSeconds(0.1f);
+        move = true;
+    }
+
+    IEnumerator CameraUp()
+    {
+        float y = transform.position.y;
+        while(transform.position.y != y + offset.y * 2)
+        {
+            transform.position += new Vector3(0,1,0);
+            yield return null;
+        }
+        playerP.transform.position += new Vector3(0,1,0);
+        yield return new WaitForSeconds(0.1f);
+        move = true;
+    }
+
+    IEnumerator CameraDown()
+    {
+        float y = transform.position.y;
+        while(transform.position.y != y - offset.y * 2)
+        {
+            transform.position += new Vector3(0,-1,0);
+            yield return null;
+        }
+        playerP.transform.position += new Vector3(0,-1,0);
+        yield return new WaitForSeconds(0.1f);
+        move = true;
     }
 }
--- yesterday-shooting/Assets/02.Scripts/JUN/PlayButtonManager.cs
+++ yesterday-shooting/Assets/02.Scripts/JUN/PlayButtonManager.cs
@@ -8,15 +8,18 @@ public class PlayButtonManager : MonoBehaviour
 {
     [SerializeField] GameObject stopPanel;
     [SerializeField] GameObject player;
+    [SerializeField] GameObject cameraP;
 
     private void Start()
     {
         player.transform.position = DataManager.instance.nowPlayer.playerPosition;
+        cameraP.transform.position = DataManager.instance.nowPlayer.cameraPosition;
     }
 
     public void OnClickExit()
     {
         DataManager.instance.nowPlayer.playerPosition = player.transform.position;
+        DataManager.instance.nowPlayer.cameraPosition = cameraP.transform.position;
         DataManager.instance.SaveData();
         SceneManager.LoadScene("Intro");
         Time.timeScale = 1;
