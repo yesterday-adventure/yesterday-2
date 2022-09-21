28802a2c2fd471db41f4e8dc6ecbbecf8d6e6831
June060522 <a80336234@gmail.com>
2022-09-21 18:09:12 +0900

    enemycolorChage

--- yesterday-shooting/Assets/02.Scripts/JUN/Hp/EnemyHp.cs
+++ yesterday-shooting/Assets/02.Scripts/JUN/Hp/EnemyHp.cs
@@ -2,14 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
+using DG.Tweening;
 public class EnemyHp : MonoBehaviour, IDamageable
 {
     [SerializeField]public float hp;
+    private SpriteRenderer _renderer;
+    private void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
     public void OnDamage(Action lambda)
     {
         hp--;
-        lambda?.Invoke();
+        if(hp > 0)
+        {
+            _renderer.DOColor(Color.red,0.2f);
+            Invoke("WhiteColorChange",0.2f);
+        }
+    }
+
+    private void WhiteColorChange()
+    {
+        _renderer.DOColor(Color.white,0.1f);
     }
 
     private void Update()
