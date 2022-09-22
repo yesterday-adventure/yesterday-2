9fc8384c968259f06997fd58155c4efba3aba79a
ahslxhvhapt12 <likjhee1@gmail.com>
2022-09-22 20:20:43 +0900

    Fix
    Fix_Door

--- yesterday-shooting/Assets/02.Scripts/JUN/CameraMove.cs
+++ yesterday-shooting/Assets/02.Scripts/JUN/CameraMove.cs
@@ -5,44 +5,64 @@ using DG.Tweening;
 
 public class CameraMove : MonoBehaviour
 {
+    [SerializeField] DoorOnOff doorOnOff;
+
     public Transform player;
     public Vector3 offset;
 
     public GameObject playerP;
     private bool move = true;
+
+    private void Awake()
+    {
+        if(doorOnOff == null)
+        {
+            doorOnOff = GetComponent<DoorOnOff>();
+        }    
+    }
+
     void Update()
     {
-        if(player.position.x - transform.position.x > offset.x && move)
+        if (player.position.x - transform.position.x > offset.x && move)
         {
             StartCoroutine(CameraRight());
+            doorOnOff.dir = Dir.right;
+            doorOnOff.changePos();
             move = false;
         }
-        if(player.position.x - transform.position.x < -offset.x && move)
+        if (player.position.x - transform.position.x < -offset.x && move)
         {
             StartCoroutine(CameraLeft());
+            doorOnOff.dir = Dir.left;
+            doorOnOff.changePos();
             move = false;
         }
-        if(player.position.y - transform.position.y > offset.y && move)
+        if (player.position.y - transform.position.y > offset.y && move)
         {
             StartCoroutine(CameraUp());
+            doorOnOff.dir = Dir.up;
+            doorOnOff.changePos();
             move = false;
         }
-        if(player.position.y - transform.position.y < -offset.y && move)
+        if (player.position.y - transform.position.y < -offset.y && move)
         {
             StartCoroutine(CameraDown());
+            doorOnOff.dir = Dir.down;
+            doorOnOff.changePos();
             move = false;
         }
+
     }
 
     IEnumerator CameraRight()
     {
         float x = transform.position.x;
-        while(transform.position.x != x + offset.x * 2)
+        while (transform.position.x != x + offset.x * 2)
         {
-            transform.position += new Vector3(1,0,0);
+            transform.position += new Vector3(1, 0, 0);
             yield return null;
         }
-        playerP.transform.position += new Vector3(1,0,0);
+        playerP.transform.position += new Vector3(1, 0, 0);
         yield return new WaitForSeconds(0.1f);
         move = true;
     }
@@ -50,12 +70,12 @@ public class CameraMove : MonoBehaviour
     IEnumerator CameraLeft()
     {
         float x = transform.position.x;
-        while(transform.position.x != x - offset.x * 2)
+        while (transform.position.x != x - offset.x * 2)
         {
-            transform.position += new Vector3(-1,0,0);
+            transform.position += new Vector3(-1, 0, 0);
             yield return null;
         }
-        playerP.transform.position += new Vector3(-1,0,0);
+        playerP.transform.position += new Vector3(-1, 0, 0);
         yield return new WaitForSeconds(0.1f);
         move = true;
     }
@@ -63,12 +83,12 @@ public class CameraMove : MonoBehaviour
     IEnumerator CameraUp()
     {
         float y = transform.position.y;
-        while(transform.position.y != y + offset.y * 2)
+        while (transform.position.y != y + offset.y * 2)
         {
-            transform.position += new Vector3(0,1,0);
+            transform.position += new Vector3(0, 1, 0);
             yield return null;
         }
-        playerP.transform.position += new Vector3(0,1,0);
+        playerP.transform.position += new Vector3(0, 1, 0);
         yield return new WaitForSeconds(0.1f);
         move = true;
     }
@@ -76,12 +96,12 @@ public class CameraMove : MonoBehaviour
     IEnumerator CameraDown()
     {
         float y = transform.position.y;
-        while(transform.position.y != y - offset.y * 2)
+        while (transform.position.y != y - offset.y * 2)
         {
-            transform.position += new Vector3(0,-1,0);
+            transform.position += new Vector3(0, -1, 0);
             yield return null;
         }
-        playerP.transform.position += new Vector3(0,-1,0);
+        playerP.transform.position += new Vector3(0, -1, 0);
         yield return new WaitForSeconds(0.1f);
         move = true;
     }
