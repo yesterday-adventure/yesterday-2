6147ff015c410de55db21f2c7187cc0433f793f2
June060522 <a80336234@gmail.com>
2022-10-12 20:30:23 +0900

    minimap

--- yesterday-shooting/Assets/02.Scripts/Map/Map.cs
+++ yesterday-shooting/Assets/02.Scripts/Map/Map.cs
@@ -7,8 +7,17 @@ public class Map : MonoBehaviour
     public int xIndex = 18;
     public int yIndex = 10;
 
+    public float _MxIndex = 0.04f;
+    public float _MyIndex = 0.08f;
+
     public Vector2 SetPos(int _xIndex, int _yIndex)
     {
         return new Vector2(xIndex * _xIndex, yIndex * _yIndex);
     }
+
+    public Vector2 MiniMapSetPos(float _xIndex,float _yIndex)
+    {
+        return new Vector2(_MxIndex * _xIndex, _MyIndex * _yIndex);
+    }
+
 }
