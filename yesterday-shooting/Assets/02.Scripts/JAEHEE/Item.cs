04116b79be8a9fd843ca07f774313a79c20baf4c
ahslxhvhapt12 <likjhee1@gmail.com>
2022-10-12 20:00:16 +0900

    Add
    Add_WeaponChange

---
+++ yesterday-shooting/Assets/02.Scripts/JAEHEE/Item.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    SpriteRenderer _renderer;
+    private void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        _renderer.sprite = Resources.Load<Sprite>(gameObject.name);
+    }
+}
