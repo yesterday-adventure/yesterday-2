3fd1e7e3c7ece3adbd18a8f3290e6350c908daae
ahslxhvhapt12 <likjhee1@gmail.com>
2022-12-13 14:24:01 +0900

    Boss
    Boss_FX
    Boss_Pattern

--- yesterday-shooting/Assets/02.Scripts/Boss/Boss.cs
+++ yesterday-shooting/Assets/02.Scripts/Boss/Boss.cs
@@ -6,10 +6,58 @@ public class Boss : MonoBehaviour
 {
     [SerializeField] float hp;
     [SerializeField] float speed;
+    [SerializeField] float distance;
+
+
+    Rigidbody rb;
+    Animator _animator;
+    GameObject player;
+
+    private void Awake()
+    {
+        _animator = transform.GetChild(0).GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        player = FindObjectOfType<PlayerHp>().transform.gameObject;
+        StartCoroutine(Pattern());
+    }
 
     private void Update()
     {
-        
+        distance = Vector2.Distance(transform.position, player.transform.position);
+    }
+
+    private IEnumerator Pattern()
+    {
+        while (hp > 0)
+        {
+            if (distance >= 7)
+            {
+                //대기
+                yield return new WaitForSeconds(0.1f);
+            }
+            else
+            {
+                if (distance < 2)
+                {
+                    //직접공격
+                }
+                else if (distance > 4)
+                {
+                    //소환
+                }
+                else
+                {
+                    while (distance > 4 || distance < 2)
+                    {
+                        rb.velocity = transform.position - player.transform.position;
+                    }
+                }
+            }
+        }
     }
 
 }
--- yesterday-shooting/Assets/02.Scripts/Boss/BossLightChange.cs
+++ yesterday-shooting/Assets/02.Scripts/Boss/BossLightChange.cs
@@ -8,13 +8,14 @@ public class BossLightChange : MonoBehaviour
     public int speed = 1;
     private float degree = 0;
     public float inten;
-    [SerializeField] private Light2D light;
+    [SerializeField] private Light2D _light;
 
     void Update()
     {
+        //이거 왜 작동하는거지
         degree += speed * Time.deltaTime;
         float radian = degree * Mathf.PI / 180; //라디안값
         inten = (Mathf.Cos(radian) + 1) / 2 * 1.2f;
-        light.intensity = inten;
+        _light.intensity = inten;
     }
 }
