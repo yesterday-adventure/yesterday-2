448fc0efa11bfe64e4c7f1758c660a92b1e19f9a
tjfdk <tjfdk6201@gmail.com>
2022-12-21 20:32:16 +0900

    아이템드롭, 겜오버씬

--- yesterday-shooting/Assets/02.Scripts/Item/DevilsBook.cs
+++ yesterday-shooting/Assets/02.Scripts/Item/DevilsBook.cs
@@ -7,7 +7,7 @@ public class DevilsBook : ItemSkil
     [SerializeField] private int itemMaxColl = 10;
     PlayerData playerData;
     BulletInfo bulletInfo;
-    public override void Skil() {
+    public override void Skil() {   
         maxColl = itemMaxColl; //이 아이템의 최대 쿨타임 수를 정한다.
         //curColl = EnemyHP 스크립트에서 에너미가 죽을 때 마다 ++ 해준 변수를 넣어준다.
         if (curColl > maxColl) {
