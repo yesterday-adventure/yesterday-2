31fb2cf624c9ab424f6c5ab112d64c0807f6168d
June060522 <a80336234@gmail.com>
2022-12-21 20:30:30 +0900

    teleportPotion

--- yesterday-shooting/Assets/02.Scripts/Item/ItemSkil.cs
+++ yesterday-shooting/Assets/02.Scripts/Item/ItemSkil.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class ItemSkil
+public abstract class ItemSkil : MonoBehaviour
 {
     public int maxColl;
     public abstract void Skil();
