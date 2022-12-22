b4750d2ac5e8393299f27504560772d5336f1702
ahslxhvhapt12 <likjhee1@gmail.com>
2022-12-22 18:14:42 +0900

    Ctlrl + K + D

--- yesterday-shooting/Assets/02.Scripts/Item/TeleportPotion.cs
+++ yesterday-shooting/Assets/02.Scripts/Item/TeleportPotion.cs
@@ -6,22 +6,22 @@ public class TeleportPotion : ItemSkil
 {
     [SerializeField] private int itemMaxColl = 10;
     int[,] randomMap;
-    
+
     public override bool Skill()
     {
-        if(DoorOnOff.Instance.hit != null)
+        if (DoorOnOff.Instance.hit != null)
             return false;
-        
-        while(true)
+
+        while (true)
         {
-            int x = Random.Range(0,10);
-            int y = Random.Range(0,10);
+            int x = Random.Range(0, 10);
+            int y = Random.Range(0, 10);
             Debug.Log(RandomMapSpawn.Instance);
-            if(Mathf.Abs(x - DoorOnOff.Instance.x) + Mathf.Abs(y - DoorOnOff.Instance.y) >= 2)
+            if (Mathf.Abs(x - DoorOnOff.Instance.x) + Mathf.Abs(y - DoorOnOff.Instance.y) >= 2)
             {
-                if(RandomMapSpawn.Instance.mapGrid[x,y] != null)
+                if (RandomMapSpawn.Instance.mapGrid[x, y] != null)
                 {
-                    GameObject.Find("Player").transform.position = RandomMapSpawn.Instance.maps.SetPos(x,y);
+                    GameObject.Find("Player").transform.position = RandomMapSpawn.Instance.maps.SetPos(x, y);
                     break;
                 }
             }
