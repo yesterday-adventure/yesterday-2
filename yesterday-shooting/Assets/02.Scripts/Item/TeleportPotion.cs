3f48a948609d9403597ef1b849e512b296a50988
June060522 <a80336234@gmail.com>
2022-12-29 22:46:10 +0900

    teleportBug, shop Price bug fixed

--- yesterday-shooting/Assets/02.Scripts/Item/TeleportPotion.cs
+++ yesterday-shooting/Assets/02.Scripts/Item/TeleportPotion.cs
@@ -16,37 +16,39 @@ public class TeleportPotion : ItemSkil
         {
             int x = Random.Range(0, 10);
             int y = Random.Range(0, 10);
-            Debug.Log(RandomMapSpawn.Instance);
             if (Mathf.Abs(x - DoorOnOff.Instance.x) + Mathf.Abs(y - DoorOnOff.Instance.y) >= 2)
             {
                 if (RandomMapSpawn.Instance.mapGrid[x, y] != null)
                 {
-                    int randomValue = Random.Range(0,4);
-                    switch(randomValue)
+                    int randomValue = Random.Range(0, 4);
+                    switch (randomValue)
                     {
                         case 0:
-                            GameObject.Find("Player").transform.position = RandomMapSpawn.Instance.maps.SetPos(x,y)
-                            +new Vector2(8f,0);
+                            GameObject.Find("Player").transform.position = RandomMapSpawn.Instance.maps.SetPos(x, y)
+                            + new Vector2(8f, 0);
                             break;
 
                         case 1:
-                            GameObject.Find("Player").transform.position = RandomMapSpawn.Instance.maps.SetPos(x,y)
-                            +new Vector2(-8f,0);
+                            GameObject.Find("Player").transform.position = RandomMapSpawn.Instance.maps.SetPos(x, y)
+                            + new Vector2(-8f, 0);
                             break;
 
                         case 2:
-                            GameObject.Find("Player").transform.position = RandomMapSpawn.Instance.maps.SetPos(x,y)
-                            +new Vector2(0,4f);
+                            GameObject.Find("Player").transform.position = RandomMapSpawn.Instance.maps.SetPos(x, y)
+                            + new Vector2(0, 4f);
                             break;
 
                         case 3:
-                            GameObject.Find("Player").transform.position = RandomMapSpawn.Instance.maps.SetPos(x,y)
-                            +new Vector2(0,-4f);
+                            GameObject.Find("Player").transform.position = RandomMapSpawn.Instance.maps.SetPos(x, y)
+                            + new Vector2(0, -4f);
                             break;
                     }
-                    Debug.Log(randomValue);
-                    GameObject.Find("Main Camera").transform.position = RandomMapSpawn.Instance.maps.SetPos(x,y);
-                    GameObject.Find("Main Camera").transform.position += new Vector3(0,0,-10);
+                    GameObject.Find("Main Camera").transform.position = RandomMapSpawn.Instance.maps.SetPos(x, y);
+                    GameObject.Find("Main Camera").transform.position += new Vector3(0, 0, -10);
+                    DoorOnOff.Instance.x = x;
+                    DoorOnOff.Instance.y = y;
+                    DataManager.instance.nowPlayer.x = x;
+                    DataManager.instance.nowPlayer.y = y;
                     break;
                 }
             }
