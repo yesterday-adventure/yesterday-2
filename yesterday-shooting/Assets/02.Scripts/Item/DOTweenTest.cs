ea74f8326b5ccfd83ded25ae783b32d57ee2746c
tjfdk <tjfdk6201@gmail.com>
2023-01-03 16:30:39 +0900

    ㄷㅏㅌㅡㅇㅜㅣㄴㅇㅕㄴㅅㅡㅂ

--- yesterday-shooting/Assets/02.Scripts/Item/DOTweenTest.cs
+++ yesterday-shooting/Assets/02.Scripts/Item/DOTweenTest.cs
@@ -7,8 +7,11 @@ public class DOTweenTest : MonoBehaviour
 {
     [SerializeField] private Ease ease;
 
+    [SerializeField] Vector2 pos;
     private void Start() {
-        ItemDropAnim();
+        if (!gameObject.CompareTag("Enemy")) transform.DOMove(pos, 5f);
+        else { transform.DOMove(pos, 5f).SetEase(ease); }
+        // ItemDropAnim();
     }
     public void ItemDropAnim() {
         transform.DOJump(transform.position, 3, 1, 2f, false).SetEase(ease);
