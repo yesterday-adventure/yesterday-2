3b1e08cf1c28f99f10f4607bc06528f871eee7d1
June060522 <a80336234@gmail.com>
2023-01-05 00:33:40 +0900

    dotDeal bug fixed

--- yesterday-shooting/Assets/02.Scripts/Enemy/DamageOverTime.cs
+++ yesterday-shooting/Assets/02.Scripts/Enemy/DamageOverTime.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class DamageOverTime : MonoBehaviour
 {
-
-    public IEnumerator Dot(float damage, float loopTime, float tick)
+    public void Dot(float damage, float loopTime, float tick)
     {
+        StartCoroutine(CoDot(damage,loopTime,tick));
+    }
+    public IEnumerator CoDot(float damage, float loopTime, float tick)
+    {
+        Debug.Log("후후 공격시작이닷");
         EnemyHp enemyHp = GetComponent<EnemyHp>();
         for (int i = 0; i < loopTime; i++)
         {
             enemyHp.OnDamage(() => { }, damage);
             yield return new WaitForSeconds(tick);
+            Debug.Log("틱이닷!!!!아얏!!");
         }
     }
 }
