7c8cb6b09becba2e0fb04680a4b00b3911a1f0a8
June060522 <a80336234@gmail.com>
2023-01-06 01:34:34 +0900

    monsterFixed

---
+++ yesterday-shooting/Assets/BattleWorm.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleWorm : MonoBehaviour
+{
+    private GameObject player;
+    private SpriteRenderer spriteRenderer;
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if (player == null)
+            return;
+
+        if (player.transform.position.x > transform.position.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (player.transform.position.x < transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+}
