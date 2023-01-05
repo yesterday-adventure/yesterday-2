2987f92760f5fca668ce1f873a21adfd4e075c6f
June060522 <a80336234@gmail.com>
2023-01-05 23:27:43 +0900

    진짜해결

--- yesterday-shooting/Assets/02.Scripts/Item/ItemCoolShow.cs
+++ yesterday-shooting/Assets/02.Scripts/Item/ItemCoolShow.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class ItemCoolShow : MonoBehaviour
 {
     [SerializeField] Image image;
+    [SerializeField] Image image1;
 
     private void Awake()
     {
@@ -16,9 +17,12 @@ public class ItemCoolShow : MonoBehaviour
         if(PlayerItem.Instance.item == null)
         {
             image.enabled = false;
+            image1.enabled = false;
         }
         else
         {
+            image1.enabled = true;
+            image.enabled = true;
             if(PlayerItem.Instance.item.ItemSkill.maxCool != 0)
                 image.fillAmount = 1 - PlayerItem.Instance.cool / PlayerItem.Instance.item.ItemSkill.maxCool;
         }
