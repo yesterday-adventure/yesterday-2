c7c87720c1d857c8b83a5280d73f70d0665287f7
ahslxjvhapt12 <102218989+ahslxjvhapt12@users.noreply.github.com>
2023-01-06 19:06:20 +0900

    Fix
    Boss

--- yesterday-shooting/Assets/02.Scripts/Player/Bullet/BulletEffect/ThunderBulletEffect.cs
+++ yesterday-shooting/Assets/02.Scripts/Player/Bullet/BulletEffect/ThunderBulletEffect.cs
@@ -6,17 +6,23 @@ public class ThunderBulletEffect : MonoBehaviour
 {
     [SerializeField] float effectRadius;
     [SerializeField] float thunderEffectDamage;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
             Debug.Log("ThunderBulletEffect");
             Collider2D[] hit = Physics2D.OverlapCircleAll(transform.position, effectRadius, Define.monster);
+            
             foreach (Collider2D item in hit)
             {
                 item.gameObject.GetComponent<EnemyHp>().OnDamage(() => {}, thunderEffectDamage);
                 //PoolManager.Instance.Pop(¿Ã∆Â∆Æ «¡∏Æ∆’,item.transform.position,Quaternion.identity);
             }
+        }
+        if (collision.gameObject.CompareTag("Boss"))
+        {
+            collision.GetComponent<Stage1_1Boss>().HP -= thunderEffectDamage;
         }
     }
 }
